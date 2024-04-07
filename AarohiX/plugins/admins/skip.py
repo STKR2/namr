3c2d0bc918a25b7460fa248e0b9bca14f3f772ca
Wa <122730917+STKR2@users.noreply.github.com>
2024-04-07 05:07:07 +0300

    Update skip.py

--- AarohiX/plugins/admins/skip.py
+++ AarohiX/plugins/admins/skip.py
@@ -11,6 +11,7 @@ from AarohiX.utils.inline import close_markup, stream_markup
 from AarohiX.utils.stream.autoclear import auto_clean
 from AarohiX.utils.thumbnails import get_thumb
 from config import Muntazer, BANNED_USERS
+from pyrogram.errors import ChatWriteForbidden
 from pyrogram.errors import UserNotParticipant
 
 async def get_channel_title(client, channel_id):

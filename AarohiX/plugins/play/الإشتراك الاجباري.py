7ca4762dfaf2e5d4be66490c3aeb3371f8dd1406
Wa <122730917+STKR2@users.noreply.github.com>
2024-04-07 20:04:33 +0300

    Update الإشتراك الاجباري.py

--- AarohiX/plugins/play/الإشتراك الاجباري.py
+++ AarohiX/plugins/play/الإشتراك الاجباري.py
@@ -22,7 +22,7 @@ async def must_join_channel(app: Client, msg: Message):
                     f"~︙عزيزي {msg.from_user.mention} \n~︙عليك الأشتراك في قناة البوت \n~︙قناة البوت : @{Muntazer}.",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("< Team Freedom >", url=link)]
+                        [InlineKeyboardButton("< Source Plus >", url=link)]
                     ])
                 )
                 await msg.stop_propagation()

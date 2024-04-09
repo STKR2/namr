9fc5bdcdadb2c051732c23d22a3a86f161be1490
Wa <122730917+STKR2@users.noreply.github.com>
2024-04-09 19:37:55 +0300

    Update stop.py

--- AarohiX/plugins/admins/stop.py
+++ AarohiX/plugins/admins/stop.py
@@ -25,7 +25,7 @@ async def must_join_channel(cli, msg: Message):
                 link = chat_info.invite_link 
             try: 
                 await msg.reply( 
-                    f"~︙عزيزي {msg.from_user.mention} \n~︙عليك الأشتراك في قناة البوت \n~︙قناة البوت : @{Muntazer}.", 
+                    f"~︙عليك الأشتراك في قناة البوت \n~︙قناة البوت : @{Muntazer}.", 
                     disable_web_page_preview=True, 
                     reply_markup=InlineKeyboardMarkup([ 
                         [InlineKeyboardButton("< Source Plus >", url=link)] 

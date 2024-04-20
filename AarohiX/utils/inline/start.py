09ddb08c9c5dbf65931e3718e392cbbbc7a36dbb
Wa <122730917+STKR2@users.noreply.github.com>
2024-04-20 17:17:28 +0300

    Update start.py

--- AarohiX/utils/inline/start.py
+++ AarohiX/utils/inline/start.py
@@ -28,5 +28,5 @@ def private_panel(_):
                     InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),                    
                 ],
             ]
-        ),
-    return buttons
+    
+     return buttons

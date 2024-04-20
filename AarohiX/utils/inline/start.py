b3d6e65a36fcf8fe69d8fa095048572715cb778c
Wa <122730917+STKR2@users.noreply.github.com>
2024-04-20 17:20:03 +0300

    Update start.py

--- AarohiX/utils/inline/start.py
+++ AarohiX/utils/inline/start.py
@@ -18,15 +18,23 @@ def start_panel(_):
 
 def private_panel(_):
     buttons = [
-                [
-                    InlineKeyboardButton(text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true"),
-                ],[
-                    InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
-                ],[
-                    InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-                    InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),                    
-                ],
-            ]
-    
-     return buttons
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(text="        " + _["S_B_4"] + "        ", callback_data="settings_back_helper"),
+        ],
+        [
+            InlineKeyboardButton(text="        " + _["S_B_2"] + "        ", url=config.SUPPORT_CHAT),
+        ],
+        [
+            InlineKeyboardButton(text="        " + _["S_B_5"] + "        ", user_id=config.OWNER_ID),
+        ],
+        [
+            InlineKeyboardButton("「 سرعة البوت 」", callback_data="bot_info_data"),
+        ],
+    ]
+    return buttons

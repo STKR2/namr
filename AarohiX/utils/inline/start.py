8996310ea1cb5a474f1b5e83766bfa8c5fa277bd
Wa <122730917+STKR2@users.noreply.github.com>
2024-05-25 22:35:21 +0300

    Update start.py

--- AarohiX/utils/inline/start.py
+++ AarohiX/utils/inline/start.py
@@ -1,8 +1,9 @@
-from typing import Union
-from AarohiX.utils.inline import first_page, private_panel, start_pannel, alive_panel
-from pyrogram.types import InlineKeyboardButton
-from config import OWNER_ID, SUPPORT_CHANNEL, SUPPORT_CHAT, BOT_USERNAME
+
 from AarohiX import app
+from typing import Union
+from pyrogram.types import InlineKeyboardButton
+from config import SUPPORT_CHANNEL, SUPPORT_CHAT, OWNER_ID
+
 
 def start_panel(_):
     buttons = [
@@ -32,8 +33,9 @@ def start_panel(_):
             )
     return buttons
 
-def private_panel(_, BOT_USERNAME, OWNER_ID: Union[bool, int] = None):
-    buttons = [[InlineKeyboardButton(text=_["S_B_8"], callback_data="settings_back_helper")]]
+
+def private_panel(_, OWNER_ID: Union[bool, int] = None):
+    buttons = [[InlineKeyboardButton(text=_["S_B_8"], callback_data="shikharbro")]]
     if SUPPORT_CHANNEL and SUPPORT_CHAT:
         buttons.append(
             [
@@ -54,7 +56,7 @@ def private_panel(_, BOT_USERNAME, OWNER_ID: Union[bool, int] = None):
         [
             InlineKeyboardButton(
                 text=_["S_B_5"],
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                url=f"https://t.me/{SUPPORT_CHANNEL}?startgroup=true",
             )
         ]
     )
@@ -62,14 +64,14 @@ def private_panel(_, BOT_USERNAME, OWNER_ID: Union[bool, int] = None):
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER_ID),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"{OWNER_ID}"),
+                InlineKeyboardButton(text=_["S_B_6"], url=f"{SUPPORT_CHANNEL}"),
             ]
         )
     else:
-        if OWNER_ID:
+        if SUPPORT_CHANNEL:
             buttons.append(
                 [
-                    InlineKeyboardButton(text=_["S_B_6"], url=f"{OWNER_ID}"),
+                    InlineKeyboardButton(text=_["S_B_6"], url=f"{SUPPORT_CHANNEL}"),
                 ]
             )
         if OWNER_ID:
@@ -80,6 +82,7 @@ def private_panel(_, BOT_USERNAME, OWNER_ID: Union[bool, int] = None):
             )
     buttons.append([InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")])
     return buttons
+
 
 def alive_panel(_):
     buttons = [

ea39359bed48c22da6ebb6493fd89a817a404a1c
Wa <122730917+STKR2@users.noreply.github.com>
2024-05-25 22:05:19 +0300

    Update start.py

--- AarohiX/utils/inline/start.py
+++ AarohiX/utils/inline/start.py
@@ -1,11 +1,21 @@
+#
+# Copyright (C) 2024-present by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
 
-from AarohiX import app
 from typing import Union
+
 from pyrogram.types import InlineKeyboardButton
-from config import SUPPORT_CHANNEL, SUPPORT_CHAT, OWNER_ID
+from config import OWNER_ID, SUPPORT_CHANNEL, SUPPORT_CHAT, BOT_USERNAME
+from AarohiX import app
 
 
-def start_panel(_):
+def start_pannel(_):
     buttons = [
         [
             InlineKeyboardButton(
@@ -34,8 +44,8 @@ def start_panel(_):
     return buttons
 
 
-def private_panel(_, OWNER_ID: Union[bool, int] = None):
-    buttons = [[InlineKeyboardButton(text=_["S_B_8"], callback_data="settings_back_helper")]]
+def private_panel(_, BOT_USERNAME, OWNER_ID: Union[bool, int] = None):
+    buttons = [[InlineKeyboardButton(text=_["S_B_8"], callback_data="settings_back_helper"]]
     if SUPPORT_CHANNEL and SUPPORT_CHAT:
         buttons.append(
             [
@@ -56,11 +66,11 @@ def private_panel(_, OWNER_ID: Union[bool, int] = None):
         [
             InlineKeyboardButton(
                 text=_["S_B_5"],
-                url=f"https://t.me/{SUPPORT_CHAT}?startgroup=true",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ]
     )
-    if SUPPORT_CHANNEL and OWNER_ID:
+        if SUPPORT_CHANNEL and OWNER:
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER_ID),
@@ -68,7 +78,7 @@ def private_panel(_, OWNER_ID: Union[bool, int] = None):
             ]
         )
     else:
-        if SUPPORT_CHANNEL:
+        if OWNER_ID:
             buttons.append(
                 [
                     InlineKeyboardButton(text=_["S_B_6"], url=f"{OWNER_ID}"),

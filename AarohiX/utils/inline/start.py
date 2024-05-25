463a997933c6a7a39d8bc182918ea92bf46910cc
Wa <122730917+STKR2@users.noreply.github.com>
2024-05-25 23:19:11 +0300

    Update start.py

--- AarohiX/utils/inline/start.py
+++ AarohiX/utils/inline/start.py
@@ -3,19 +3,28 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton
 from config import SUPPORT_CHANNEL, SUPPORT_CHAT, OWNER_ID
 
+
 def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
                 text=_["S_B_1"],
                 url=f"https://t.me/{app.username}?start=help",
-            ),
-            InlineKeyboardButton(text=_["S_B_2"], callback_data="settings_helper"),
-            InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_CHAT}")
+            )
         ],
         [
+            InlineKeyboardButton(text=_["S_B_2"], callback_data="settings_helper"),
             InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"),
-        ]
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_CHAT}"),
+            InlineKeyboardButton(
+                text="< المطور >", url="https://t.me/{OWNER_ID}"
+            ),
+            InlineKeyboardButton(
+                text="< تنصيب بوت >", url="https://t.me/{OWNER_ID}"
+            ),
+        ],
     ]
     return buttons
 
@@ -25,23 +34,28 @@ def private_panel(_, OWNER_ID: Union[bool, int] = None):
         [InlineKeyboardButton(text=_["S_B_8"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"),
-            InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_CHAT}")
+            InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_CHAT}"),
         ],
         [
             InlineKeyboardButton(
                 text=_["S_B_5"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
-        ]
+        ],
+        [
+            InlineKeyboardButton(
+                text="< المطور >", url="https://t.me/{OWNER_ID}"
+            ),
+            InlineKeyboardButton(
+                text="< تنصيب بوت >", url="https://t.me/{OWNER_ID}"
+            ),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/{OWNER_ID}"),
+        ] if OWNER_ID else [],
+        [InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")],
     ]
-    if OWNER_ID:
-        buttons.append(
-            [
-                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER_ID),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/{OWNER_ID}"),
-            ]
-        )
-    buttons.append([InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")])
     return buttons
 
 
@@ -52,6 +66,6 @@ def alive_panel(_):
                 text="- اضفني .", url=f"https://t.me/{app.username}?startgroup=true"
             ),
             InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_CHAT}"),
-        ]
+        ],
     ]
     return buttons

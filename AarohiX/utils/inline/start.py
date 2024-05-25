82b7fcde45b2f5e9c81b092cb1e58c598cfd2af6
Wa <122730917+STKR2@users.noreply.github.com>
2024-05-25 21:18:00 +0300

    Update start.py

--- AarohiX/utils/inline/start.py
+++ AarohiX/utils/inline/start.py
@@ -1,25 +1,11 @@
-from pyrogram.types import InlineKeyboardButton
 
-import config
 from AarohiX import app
-
-
-#
-# Copyright (C) 2024-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 from typing import Union
 from pyrogram.types import InlineKeyboardButton
-from config import SUPPORT_CHANNEL, SUPPORT_CHAT
+from config import SUPPORT_CHANNEL, SUPPORT_CHAT, OWNER_ID
 
 
-def start_pannel(_):
+def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
